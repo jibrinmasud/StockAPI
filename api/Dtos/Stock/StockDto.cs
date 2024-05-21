97a7dd354d40e8b0d8b33809f327d2bd8d92aa80
jibrinmasud <jibrinmasud494@gmail.com>
2024-05-21 10:31:31 +0100

    Adding Dtos and mappers

---
+++ api/Dtos/Stock/StockDto.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Stock
+{
+    public class StockDto
+    {
+         public int Id { get; set;}
+        public string? Symbol { get; set; }
+        public string? CompanyName { get; set; }
+       
+        public decimal Purchase { get; set; }
+        
+        public decimal LastDiv { get; set; }
+        public string? Industry { get; set; }
+        public long MarketCap { get; set; }     
+    }
+}
