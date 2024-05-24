a30321a185af998b24bc45410784b3bf8398969f
jibrinmasud <jibrinmasud494@gmail.com>
2024-05-24 10:04:55 +0100

    Added the Register User Endpoint Using JWT

---
+++ api/Dtos/Account/RegisterDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Account
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string? UserName { get; set; }
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public string? PassWord { get; set; }
+    }
+}
---
+++ api/Migrations/20240524084538_SeedRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "7edc84cd-9625-4f2a-adb4-68e9baca3ed5", null, "User", "USER" },
+                    { "fefda50e-15b3-4d51-8564-90a0e554f3e3", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7edc84cd-9625-4f2a-adb4-68e9baca3ed5");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fefda50e-15b3-4d51-8564-90a0e554f3e3");
+        }
+    }
+}
